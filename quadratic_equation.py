97838201be8057d370341252439990b1226789b4
Valery Ramusik <valer2677.@gmailcom>
2017-08-05 05:40:50 +0300

    del usless root1

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -5,10 +5,9 @@ def get_roots(a, b, c):
     discriminant = b ** 2 - 4 * a * c
     if discriminant < 0:
         return None, None
-    elif discriminant == 0:
-        root1 = (-b - sqrt(discriminant)) / (2 * a)
+    root1 = (-b - sqrt(discriminant)) / (2 * a)
+    root2 = (-b + sqrt(discriminant)) / (2 * a)
+    if discriminant == 0:
         return root1, None
     else:
-        root1 = (-b - sqrt(discriminant)) / (2 * a)
-        root2 = (-b + sqrt(discriminant)) / (2 * a)
         return root1, root2
